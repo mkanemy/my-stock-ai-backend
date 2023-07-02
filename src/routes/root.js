3e75699624f8aeb51cd85ced44bb0f408c94cf54
Matthew Kanemy <mkanemy@uwaterloo.ca>
2023-07-02 16:39:13 -0400

    get root routes running

---
+++ src/routes/root.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+// GET method route
+app.get('/', (req, res) => {
+    res.send('GET request to the homepage');
+})
+
+// POST method route
+app.post('/', (req, res) => {
+    res.send('POST request to the homepage');
+})
+
+module.exports = app;
