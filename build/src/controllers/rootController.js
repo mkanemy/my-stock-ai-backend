9c1aa5537f3c6b8a105bcf6b870b7107e06ac860
Matthew Kanemy <mkanemy@uwaterloo.ca>
2023-07-03 19:10:34 -0400

    fix gitignore

--- build/src/controllers/rootController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.rootGetController = void 0;
-function rootGetController(req, res) {
-    return 'We made it :D';
-}
-exports.rootGetController = rootGetController;
--- build/test/unit/controllers/rootController.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const root = require('../../../src/controllers/rootController');
-describe('Test Root Controller', function () {
-    test('root controller exists', () => {
-        expect(root.rootGetController()).toBe('We made it :D');
-    });
-});
