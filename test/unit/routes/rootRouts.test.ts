63e2694471c19bd6e7739665bd7f46d2dee01740
Matthew Kanemy <mkanemy@uwaterloo.ca>
2023-07-03 00:07:39 -0400

    Testing

--- test/unit/routes/rootRouts.test.ts
+++ test/unit/routes/rootRouts.test.ts
@@ -7,11 +7,17 @@ app.use('/', rootRoutes);
 
 describe('Test Root Routes', function () {
 
-    test('Test that / root route exists and returns correctly', async () => {
+    test('Test that GET / root route exists and returns correctly', async () => {
         const res = await request(app).get('/');
 
         expect(res.statusCode).toEqual(200);
         expect(res.text).toEqual('We made it :D');
+    });
+
+    test('Test that POST / root route exists and returns correctly', async () => {
+        const res = await request(app).post('/');
+
+        expect(res.text).toEqual('POST request to the homepage');
     });
 
 });
