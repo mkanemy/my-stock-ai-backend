7170028209236dd173a405082d8d82c8e4e87af6
Matthew Kanemy <mkanemy@uwaterloo.ca>
2023-07-04 23:47:14 -0400

    Add cors to connect to frontend

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,9 @@ const routes = require('./routes/rootRoutes');
 const app = express();
 const PORT = 8000;
 const swaggerDocument = require('../swagger.json');
+const cors = require('cors');
 
+app.use(cors());
 app.use("/", routes);
 
 app.listen(PORT, () => {
