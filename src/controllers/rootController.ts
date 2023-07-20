f528e931ec73c5b5c5f0309c09121eec78a7afbd
Matthew Kanemy <mkanemy@uwaterloo.ca>
2023-07-20 00:01:18 -0400

    sonarqube

--- src/controllers/rootController.ts
+++ src/controllers/rootController.ts
@@ -4,13 +4,13 @@ import { dataQuery } from "../data/dataQuery";
 export async function rootGetController(req: Request, res: Response) {
     let response = "No Result";
 
-try {
-
+    try {
         const data = await dataQuery("SELECT * FROM msaitestdb LIMIT 1");
-
         response = data[0];
-
-    } finally {
-        return response;
+    } catch (e) {
+        console.log(e);
+        response = "Error";
     }
+
+    return response;
 }
