7f3a60179f3504b391d845256b64742d889fd11d
Matthew Kanemy <mkanemy@uwaterloo.ca>
2023-07-23 22:49:06 -0400

    Connect hosted vercel DB

--- src/data/dataQuery.ts
+++ src/data/dataQuery.ts
@@ -1,19 +1,17 @@
-import { Client } from "ts-postgres/dist/src/client";
+const { Pool } = require("pg");
 const db = require('../configs/db.json');
 
 export async function dataQuery(query: string) {
-    let dataResponse = [];
+    let dataResponse: string[] = [];
 
-    const client = new Client({"host": db.host, "port": db.port, "database": db.database});
-    await client.connect();
+    const pool = new Pool({
+        connectionString: db.url + "?sslmode=require",
+    })
 
-    const result = client.query(query);
+    const result = await pool.query(query);
 
-    for await (const row of result) {
-        let rowResponse = row.get('test');
-        if (rowResponse) {
-            dataResponse.push(rowResponse.toString());
-        }
+    for await (const row of result.rows) {
+        dataResponse.push(row.test);
     }
 
     return dataResponse;
