cb31f4d553d494d62186adee873ad24b5025cdab
Matthew Kanemy <mkanemy@uwaterloo.ca>
2023-08-21 22:40:26 -0400

    Try diff index

--- build/api/index.js
+++ build/api/index.js
@@ -1,19 +1,36 @@
 "use strict";
+// import express, { Request, Response } from "express";
+// import { initialize } from "express-openapi";
+// import swaggerUi from "swagger-ui-express";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// const routes = require('./routes/rootRoutes');
+// const app = express();
+// const PORT = 8000;
+// const swaggerDocument = require('../swagger.json');
+// const cors = require('cors');
+// require('dotenv').config()
+// app.use(cors());
+// app.use("/", routes);
+// app.listen(PORT, () => {
+//     console.info(`Server listening on port ${PORT}`);
+// });
+// app.use(
+//     '/api-docs',
+//     swaggerUi.serve, 
+//     swaggerUi.setup(swaggerDocument)
+// );
 const express_1 = __importDefault(require("express"));
-const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
-const routes = require('./routes/rootRoutes');
 const app = (0, express_1.default)();
-const PORT = 8000;
-const swaggerDocument = require('../swagger.json');
-const cors = require('cors');
-require('dotenv').config();
-app.use(cors());
-app.use("/", routes);
-app.listen(PORT, () => {
-    console.info(`Server listening on port ${PORT}`);
+const port = process.env.PORT || 8080;
+app.get('/', (_req, res) => {
+    return res.send('Express Typescript on Vercel');
 });
-app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerDocument));
+app.get('/ping', (_req, res) => {
+    return res.send('pong 🏓');
+});
+app.listen(port, () => {
+    return console.log(`Server is listening on ${port}`);
+});
