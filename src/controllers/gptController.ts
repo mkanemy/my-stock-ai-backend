ebfb09658d48c54839065d4f87ef91a925d1a2b7
Matthew Kanemy <mkanemy@uwaterloo.ca>
2023-09-11 18:43:03 -0400

    Integrate Stock Analysis

--- src/controllers/gptController.ts
+++ src/controllers/gptController.ts
@@ -6,5 +6,5 @@ export async function getArticleSummary(req: Request, res: Response) {
 
     // Process input - article array & ticker
     
-    await gptSummaryService.getSummary(req.body.ticker, req.body.companyName, req.body.articles, res)
+    await gptSummaryService.getSummary(req.body.ticker, req.body.companyName, req.body.articles, req.body.quote, res)
 }
--- src/services/gptSummaryService.ts
+++ src/services/gptSummaryService.ts
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 import { Response } from "express";
 
-export async function getSummary(ticker: string, companyName: string, articles: string, res: Response) {
+export async function getSummary(ticker: string, companyName: string, articles: string, quote: any, res: Response) {
 
     const openai = new OpenAI({
         apiKey: process.env.GPT_API_KEY,
@@ -12,11 +12,17 @@ export async function getSummary(ticker: string, companyName: string, articles: 
         messages: [
             {
             "role": "system",
-            "content": "I will provide you a stock ticker symbol and name of company along with a list of urls to recent news articles about that company. Your job is to create a summary in 200 words or less summarizing the significant news points about the company for investors. Make sure that the points of significance are mentioned!"
+            "content": "I will provide you a stock ticker symbol, the corresponding name of the company, a list of urls to recent news articles related to that company and a bunch of current financial data about that company."
+            + "Your job is to firstly create a summary in 200 words or less summarizing any possible significant news points (if there are no significant news points just use any points you can find) about the company for investors. Make sure that any points of significance are mentioned!"
+            + "Next, you will create a second separate paragraph that is separated by '---' from the first one. This paragraph will be 100 words or less and will inform me - based on the news articles and financial data, if the stock is a 'Strong Buy', 'Soft Buy', 'Hold', 'Soft Sell' or 'Strong Sell'. When doing this second paragraph please don't just repeat the numbers I gave you - I already know them."
+            + "Also for formatting - YOU MUST MAKE SURE that the last number of the response is either 1 for strong buy, 2 for soft buy, 3 for hold, 4 for soft sell or 5 for strong sell (DO NOT PUT ANYTHING AFTER THIS NUMBER EVEN A PERIOD IT MUST BE THE LAST CHARACTER OF THE WHOLE RESPONSE!!!)"
             },
             {
             "role": "user",
-            "content": "Ticker: " + ticker + "\nName: " + companyName + "\nArticles: " + articles.toString()
+            "content": "Ticker: " + ticker + "\nName: " + companyName + "\nArticles: " + articles?.toString() + "\nCurrent Price: " + quote.currentPrice + 
+            "\nPrevious Open: " + quote.open + "\nDaily High: " + quote.high + "\nDaily Low: " + quote.low + "\nMarket Cap: " + quote.marketCap + 
+            "\nDividend Yield: " + quote.yield + "\nBeta: " + quote.beta + "\nEarnings Per Share: " + quote.eps + 
+            "\nPrice to Earnings Ratio: " + quote.pe
             }
         ],
         temperature: 1,
